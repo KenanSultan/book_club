8a88f186d2ff91e5bfcae825c03b246fff4ca051
ZuuZaa <zumbaghirova@gmail.com>
2019-04-17 16:36:49 +0400

    firebase initialized

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+var config = {
+    apikey: "AIzaSyAXb3Kn2gxmzUrRh1O0wlcfsNKe3uStpOM",
+    authDomain: "https://bookclub-5e3c6.firebaseapp.com/",
+    databaseURL: "https://bookclub-5e3c6.firebaseio.com/",
+    projectID: "bookclub-5e3c6"
+}
+firebase.initializeApp(config)
+var database = firebase.database()
+
+$(".sign-in-btn").on("click", function () {
+    
+    $(".section2").removeClass("d-none")
+    
+    $("#sign-up").on("submit", function (e) {
+        
+        e.preventDefault()
+        
+        database.ref("/users/").push({
+            name: $("#username").val(),
+            password: $("#password").val()
+        })
+        $(".section2").addClass("d-none")
+    })
+
+    database.ref().on("value", function (snapshot) {
+        console.log(snapshot)
+    })
+})
+
