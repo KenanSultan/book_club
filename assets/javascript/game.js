660d6bd9b59e0fd4b6cbb3113ce24c3d674eeda2
ZuuZaa <zumbaghirova@gmail.com>
2019-04-18 17:08:30 +0400

    book votes are in firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,9 +10,8 @@ firebase.initializeApp(config)
 var database = firebase.database()
 
 $(".sign-in-btn").on("click", function () {
-
+    alert("hfdkjds")
     $(".section2").removeClass("d-none")
-    
 
     $("#sign-up").on("submit", function (e) {
 
@@ -32,7 +31,7 @@ $(".sign-in-btn").on("click", function () {
         console.log(snapshot)
     })
 })
-$("#book-search").on("submit", function(e) {
+$("#book-search").on("submit", function (e) {
     e.preventDefault()
     $.ajax({
         url: "https://www.googleapis.com/books/v1/volumes",
@@ -41,25 +40,66 @@ $("#book-search").on("submit", function(e) {
         }
     }).then(function (resp) {
         console.log(resp)
-        $("#search-btn-book-image").empty() 
-        for (let i = 0; i<5; i++ ) {
 
-            var bookUsersChoise = $("<img>")
-            bookUsersChoise.attr("id","bookUsersChoise")
-            bookUsersChoise.attr("src", resp.items[i].volumeInfo.imageLinks.thumbnail)
-            var searchImageHolder = $("<div>")
-            searchImageHolder.addClass("col")
-            searchImageHolder.append(bookUsersChoise)
-            $("#search-btn-book-image").append(searchImageHolder)
-        }
+        var isbn = resp.items[0].volumeInfo.industryIdentifiers[0].identifier
+
+        database.ref("/books/").once("value", function (snapshot) {
+            var books = snapshot.val()
+            console.log(books)
+            if (!books) {
+                database.ref("/books/" + isbn).set({
+                    name: resp.items[0].volumeInfo.title,
+                    authors: resp.items[0].volumeInfo.authors[0],
+                    poster: resp.items[0].volumeInfo.imageLinks.thumbnail
+                })
+                database.ref("/votes/" + isbn).set({
+                    vote: 0
+                })
+            } else {
+                for (i in books) {
+                    if (i != isbn) {
+                        database.ref("/books/" + isbn).set({
+                            name: resp.items[0].volumeInfo.title,
+                            authors: resp.items[0].volumeInfo.authors[0],
+                            poster: resp.items[0].volumeInfo.imageLinks.thumbnail
+                        })
+                        database.ref("/vote/" + isbn).set({
+                            vote: 0
+                        })
+                    }
+                }
+            }
+        })
+        database.ref("/votes/").once("value", function (snapshot) {
+            var votes = snapshot.val()
+            console.log(votes)
+            if (!votes) {
+                database.ref("/votes/" + isbn).set({
+                    vote: 0
+                })
+            } else {
+                for (i in votes) {
+                    if (i != isbn) {
+                        database.ref("/vote/" + isbn).set({
+                            vote: 0
+                        })
+                    }
+                }
+            }
+        })
+        $("#book-search-name").text("")
     })
+
+    //resp.items[i].volumeInfo.imageLinks.thumbnail
+
+
 })
-$("#show-all-list").on("click", function(){
-    $( ".section3" ).removeClass("d-none")
-        $('html, body').animate({
-            scrollTop: $(".section3").offset().top
-        }, 2000)
-    
-     
+$("#show-all-list").on("click", function () {
+
+    $(".section3").removeClass("d-none")
+    $('html, body').animate({
+        scrollTop: $(".section3").offset().top
+    }, 2000)
+
 })
 //$("#bookUsersChoise").on("click", function() {})
