52d11280a9195944f5b04fdc7fc19b089bea5c2d
KenanSultan <kenansultan@hotmail.com>
2019-04-19 16:21:43 +0400

    [*] comment section

--- assets/javascript/discussion.js
+++ assets/javascript/discussion.js
@@ -1,7 +1,8 @@
+var isbn = ""
 
 database.ref("bookOnDiscussion").once("value", function (snapshot) {
-    console.log(snapshot.val())
     isbn = snapshot.val().isbn
+
     $("#comment-book-post").attr("src", snapshot.val().poster)
     $(".comment-book-name").html(snapshot.val().name)
     $(".comment-book-author").html(snapshot.val().author)
@@ -11,11 +12,13 @@ database.ref("bookOnDiscussion").once("value", function (snapshot) {
 
 $("#comment-submit-btn").on("submit", function (e) {
     e.preventDefault()
+    comment = $(".comment-placeholder").val()
+    console.log(comment)
 
     if (enter == true) {
-        database.ref("/books/" + isbn + "/comments").push({
+        database.ref("books/" + isbn + "/comments").push({
             username: user_name,
-            comment: $("comment-placeholder").val()
+            comment
         })
     } else {
         var alertSpace = $("<div>")
@@ -25,14 +28,14 @@ $("#comment-submit-btn").on("submit", function (e) {
 })
 
 
-database.ref("/books/" + isbn + "comments").on("value", function (snapshot) {
+database.ref("books/").on("value", function (snapshot) {
 
-    var comment = snapshot.val()
-
+    var comment = snapshot.val()[isbn].comments
+    $(".name-comment-div").empty()
     for (i in comment) {
         var nameholder = $("<div>")
         nameholder.attr("id", "comment-user-name")
-        nameholder.html(coment[i].username)
+        nameholder.html(comment[i].username)
         $(".name-comment-div").append(nameholder)
         var commentholder = $("<div>")
         commentholder.addClass("users-comments")
